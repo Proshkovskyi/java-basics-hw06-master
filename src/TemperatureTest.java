f5d76b642a86c7dcc1d9fb47f0517e1335b3483b
Proshkovskyi <ian.proshkovskyi@ringteam.com>
2020-05-05 20:38:21 +0300

    add fix

---
+++ src/TemperatureTest.java
@@ -0,0 +1,53 @@
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class TemperatureTest {
+
+    /**
+     * Test data for Fahrenheit to Celsius conversion.
+     *
+     * @return test data array
+     */
+    @DataProvider(name = "Fahrenheit")
+    public static Object[][] fahrenheit() {
+        return new Object[][]{
+                {212.0, 100.0},
+                {32.0, 0.0},
+                {-15, -26.11111111111111},
+                {-40, -40},
+                {97.9, 36.611111111111114}
+        };
+    }
+
+    /**
+     * Test data for Celsius to Fahrenheit conversion.
+     *
+     * @return test data array
+     */
+    @DataProvider(name = "Celsius")
+    public static Object[][] celsius() {
+        return new Object[][]{
+                {100.0, 212.0},
+                {0.0, 32.0},
+                {-26.11111111111111, -15},
+                {96, 204.8},
+                {-40, -40}
+        };
+    }
+
+    @Test(dataProvider = "Fahrenheit")
+    public void testFahrenheitToCelsius(double fahrenheit, double expectedResult) {
+        double actulResult = Temperature.fahrenheitToCelsius(fahrenheit);
+
+        assertEquals(actulResult, expectedResult, "Conversion from Fahrenheit to Celsius is wrong");
+    }
+
+    @Test(dataProvider = "Celsius")
+    public void testCelsiusToFahrenheit(double celsius, double expectedResult) {
+        double actulResult = Temperature.celsiusToFahrenheit(celsius);
+
+        assertEquals(actulResult, expectedResult, "Conversion from Celsius to Fahrenheit is wrong");
+    }
+}
